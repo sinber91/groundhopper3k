0887c07077ebe200f696d83d3ca2cc140cee71c3
Sindre Berntsen <Sindre@Sindres-MacBook-Air.local>
2015-09-28 14:56:05 +0200

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name groundhopper3kApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the groundhopper3kApp
+ */
+angular.module('groundhopper3kApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name groundhopper3kApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the groundhopper3kApp
+ */
+angular.module('groundhopper3kApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
