89317eaf07780398717900151c25d3a341a407ef
Sindre Berntsen <Sindre@Sindres-MacBook-Air.local>
2015-09-29 15:51:53 +0200

    added stadiums.html

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,6 +29,11 @@ angular
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
+      .when('/stadiums', {
+        templateUrl: 'views/stadiums.html'
+        //controller: 'AboutCtrl',
+        //controllerAs: 'stadium'
+      })
       .otherwise({
         redirectTo: '/'
       });
